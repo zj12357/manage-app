342e91032641e1ff5bb09922ad2f00a54cbb7702
norman <zj15355208214@gmail.com>
2023-06-17 14:22:01 +0800

    注册

--- src/components.d.ts
+++ src/components.d.ts
@@ -17,6 +17,7 @@ declare module '@vue/runtime-core' {
     VanButton: typeof import('vant/es')['Button']
     VanCell: typeof import('vant/es')['Cell']
     VanCellGroup: typeof import('vant/es')['CellGroup']
+    VanCheckbox: typeof import('vant/es')['Checkbox']
     VanEmpty: typeof import('vant/es')['Empty']
     VanField: typeof import('vant/es')['Field']
     VanForm: typeof import('vant/es')['Form']
--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import authLock from '~/assets/images/icons/icon_auth_lock.png'
+import authSafe from '~/assets/images/icons/icon_auth_safe.png'
+import authUser from '~/assets/images/icons/icon_auth_user.png'
+
 const route = useRoute()
 const router = useRouter()
 const navList = [
@@ -14,8 +18,17 @@ const navList = [
   },
 
 ]
+
+const state = reactive({
+  username: '',
+  password: '',
+
+})
 function toPage(path: string) {
   router.push(path)
+}
+function onSubmit(values: any) {
+  console.log('submit', values)
 }
 </script>
 
@@ -25,6 +38,59 @@ function toPage(path: string) {
       <div v-for="(item, index) in navList" :key="index" class="relative mr-[25px] mt-[20px] h-full text-lg font-500" :class="route.path === item.path && 'nav-active' " @click="toPage(item.path)">
         {{ item.name }}
       </div>
+    </div>
+    <div class="mt-[30px] h-[362px] w-full">
+      <van-form @submit="onSubmit">
+        <van-cell-group>
+          <van-field
+            v-model="state.username"
+            name="用户名"
+            placeholder="请填写用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]"
+          >
+            <template #label>
+              <img :src="authUser" class="relative top-[-4px] w-[28px]" alt="" />
+            </template>
+          </van-field>
+          <van-field
+            v-model="state.password"
+            type="password"
+            name="密码"
+            placeholder="请填写密码"
+            :rules="[{ required: true, message: '请填写密码' }]"
+          >
+            <template #label>
+              <img :src="authLock" class="relative top-[-4px] w-[28px]" alt="" />
+            </template>
+          </van-field>
+          <van-field
+            v-model="state.password"
+            type="password"
+            name="密码"
+            placeholder="请填写验证码"
+            :rules="[{ required: true, message: '请填写验证码' }]"
+          >
+            <template #label>
+              <img :src="authSafe" class="relative top-[-4px] w-[28px]" alt="" />
+            </template>
+          </van-field>
+          <div class="relative w-full">
+            <div class="absolute right-0">
+              <van-field name="checkbox">
+                <template #input>
+                  <van-checkbox>记住账号</van-checkbox>
+                </template>
+              </van-field>
+            </div>
+          </div>
+        </van-cell-group>
+
+        <div class="mt-[80px]">
+          <van-button round block type="primary" native-type="submit">
+            立即登录
+          </van-button>
+        </div>
+      </van-form>
     </div>
   </div>
 </template>
@@ -52,6 +118,18 @@ function toPage(path: string) {
   opacity: 0.2;
   background: $primary-color;
   border-radius: 50%;
+}
+:deep() {
+    .van-cell {
+        padding: 12px 0px;
+    }
+    .van-field__label {
+        width: auto;
+    }
+    .van-hairline--top-bottom::after{
+      border: none;
+    }
+
 }
 </style>
 
